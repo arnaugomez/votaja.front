b0410259d9963e9eb46060e57638a317c269f3c3
Arnau Gómez <agomez@mrmilu.com>
2021-10-16 15:02:18 +0200

    share poll section in poll results section

--- src/poll/view/sections/PollResults.tsx
+++ src/poll/view/sections/PollResults.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import H2 from "../../../common/view/atoms/H2";
 import { Poll } from "../../domain/models/Poll";
 import CelebrationImg from "../atoms/CelebrationImg";
+import SharePollUrl from "../molecules/SharePollUrl";
 import SimplePollStats from "../molecules/SimplePollStats";
 
 interface Props {
@@ -11,12 +12,19 @@ interface Props {
   onGoToVoteForm: () => void;
 }
 
-export default function PollResults({poll, voteId}: Props) {
+export default function PollResults({ poll, voteId }: Props) {
   return (
-    <div className="px-4 py-6 border border-gray-100 rounded-2xl">
-      <CelebrationImg />
-      <H2 className="text-center pb-4">Resultats</H2>
-      <SimplePollStats poll={poll} voteId={voteId} />
-    </div>
+    <>
+      <div className="px-4 py-6 border border-gray-100 rounded-2xl">
+        <CelebrationImg />
+        <H2 className="text-center pb-4">Resultats</H2>
+        <SimplePollStats poll={poll} voteId={voteId} />
+      </div>
+
+      <div className="mt-4 py-6 px-4 bg-gray-50 rounded-2xl">
+        <H2 className="text-center pb-4">Comparteix l&apos;enquesta</H2>
+        <SharePollUrl poll={poll} />
+      </div>
+    </>
   );
 }
