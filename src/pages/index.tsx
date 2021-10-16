57475afd0b5a3d8b4072ac0994800e47c99d9d4f
Arnau Gómez <agomez@mrmilu.com>
2021-10-16 18:38:07 +0200

    added seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { pollRepository } from "../poll/data/repositories/PollRepository";
 import { Poll } from "../poll/domain/models/Poll";
 import CreatePoll from "../poll/view/forms/CreatePoll";
 import SharePoll from "../poll/view/sections/SharePoll";
+import Head from "next/head";
 
 export default function Home() {
   const [poll, setPoll] = useState<Poll>(null);
@@ -19,6 +20,18 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>Votaja: crea una enquesta en 10 segons | VOTAJA</title>
+        <meta
+          name="description"
+          content="Votaja és el creador d'enquestes més ràpid que hi ha. Crea una enquesta en 10 segons i comparteix-la per Whatsapp o a la teva pàgina web."
+        />
+        <meta
+          name="keywords"
+          content="votaja, enquesta, enquestes, generador d'enquestes, creador d'enquestes, crea enquesta, pancripto, pancripto labs, Arnau Gómez, enquesta whatsapp, enquestes whatsapp"
+        />
+        <meta name="author" content="Arnau Gómez" />
+      </Head>
       <Hero />
       {poll ? <SharePoll poll={poll} /> : <CreatePoll onCreate={createPoll} />}
     </>
--- src/pages/v/[slug].tsx
+++ src/pages/v/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import React from "react";
 import { pollRepository } from "../../poll/data/repositories/PollRepository";
 import { IPoll, Poll } from "../../poll/domain/models/Poll";
@@ -21,9 +22,21 @@ export const getServerSideProps: GetServerSideProps<Props, { slug: string }> =
   };
 
 export default function VotePage({ poll }: Props) {
-  const p = new Poll(poll)
+  const p = new Poll(poll);
   return (
     <>
+      <Head>
+        <title>Enquesta: {poll.title} | Creador d&apos;enquestes VOTAJA</title>
+        <meta
+          name="description"
+          content="Votaja és el creador d'enquestes més ràpid que hi ha. Crea una enquesta en 10 segons i comparteix-la per Whatsapp o a la teva pàgina web."
+        />
+        <meta
+          name="keywords"
+          content="votaja, enquesta, enquestes, generador d'enquestes, creador d'enquestes, crea enquesta, pancripto, pancripto labs, Arnau Gómez, enquesta whatsapp, enquestes whatsapp"
+        />
+        <meta name="author" content="Arnau Gómez" />
+      </Head>
       <Vote poll={p} />
     </>
   );
